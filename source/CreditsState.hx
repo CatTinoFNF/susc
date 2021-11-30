d9fdb6135ddb3726995503c7af0291ec0afc9bcd
CatTinoFNF <95261169+CatTinoFNF@users.noreply.github.com>
2021-11-30 09:22:18 +0700

    Add files via upload
    i hate this

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -46,8 +46,8 @@ class CreditsState extends MusicBeatState
 		add(grpOptions);
 
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
-			['Mod Port By'],
-			['CatTino FNF',		    'cattinofnf',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['Port By'],
+			['CatTino FNF',		    'cattinofnf',		    'Main Port This Mod',	 'https://www.youtube.com/channel/UCl3dBJC3rMWchPrB9pUU3yg',	'0xFF00416a'],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
